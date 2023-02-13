c8d80dec589fd0ae80ab2dbfd70c7b05d324a9ab
MrUDDoff <kubasov.den@bk.ru>
2023-02-13 19:35:53 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Перейти к введению
+            Приступить к чтению
           </Link>
         </div>
       </div>
